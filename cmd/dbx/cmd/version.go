9f6b987596783c6118075d24ef2dfb00c17dfac7
Adam Brady <adam@boxxen.org>
2025-04-26 23:27:36 +1000

    Load new flake versioning data

--- cmd/dbx/cmd/version.go
+++ cmd/dbx/cmd/version.go
@@ -13,8 +13,17 @@ var versionCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		version := version.GetDBXRelease()
 
-		fmt.Printf("Release: %s\n", version.Release)
-		fmt.Printf("NurHash: %s\n", version.NurHash)
+		fmt.Printf("Dogebox Release: %s\n", version.Release)
+
+		fmt.Printf("Packages:\n")
+		for pkg, tuple := range version.Packages {
+			fmt.Printf(" - %s: %s (%s)\n", pkg, tuple.Rev, tuple.Hash)
+		}
+
+		if len(version.Packages) == 0 {
+			fmt.Printf("No packages found, if you're not actively developing, this is unexpected, please raise an issue.\n")
+		}
+
 		fmt.Printf("Git: %s\n", version.Git.Commit)
 		fmt.Printf("Dirty: %t\n", version.Git.Dirty)
 	},
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -12,27 +12,47 @@ type DBXVersionInfoGit struct {
 	Dirty  bool   `json:"dirty"`
 }
 
+type DBXVersionInputTuple struct {
+	Rev  string `json:"rev"`
+	Hash string `json:"hash"`
+}
+
 type DBXVersionInfo struct {
-	Release string            `json:"release"`
-	NurHash string            `json:"nurHash"`
-	Git     DBXVersionInfoGit `json:"git"`
+	Release  string                          `json:"release"`
+	Packages map[string]DBXVersionInputTuple `json:"packages"`
+	Git      DBXVersionInfoGit               `json:"git"`
 }
 
 func GetDBXRelease() *DBXVersionInfo {
 	release := "unknown"
-	nurHash := "unknown"
 
-	if dbxReleaseData, err := os.ReadFile("/opt/dbx-release"); err == nil {
+	if dbxReleaseData, err := os.ReadFile("/opt/versioning/dbx"); err == nil {
 		release = strings.TrimSpace(string(dbxReleaseData))
 	}
 
-	if nurHashData, err := os.ReadFile("/opt/dbx-nur-hash"); err == nil {
-		nurHash = strings.TrimSpace(string(nurHashData))
+	packages := make(map[string]DBXVersionInputTuple)
+	if entries, err := os.ReadDir("/opt/versioning"); err == nil {
+		for _, entry := range entries {
+			if entry.IsDir() {
+				pkgName := entry.Name()
+				var tuple DBXVersionInputTuple
+
+				if revData, err := os.ReadFile("/opt/versioning/" + pkgName + "/rev"); err == nil {
+					tuple.Rev = strings.TrimSpace(string(revData))
+				}
+
+				if hashData, err := os.ReadFile("/opt/versioning/" + pkgName + "/hash"); err == nil {
+					tuple.Hash = strings.TrimSpace(string(hashData))
+				}
+
+				packages[pkgName] = tuple
+			}
+		}
 	}
 
 	return &DBXVersionInfo{
-		Release: release,
-		NurHash: nurHash,
+		Release:  release,
+		Packages: packages,
 		Git: DBXVersionInfoGit{
 			Commit: versioninfo.Revision,
 			Dirty:  versioninfo.DirtyBuild,
