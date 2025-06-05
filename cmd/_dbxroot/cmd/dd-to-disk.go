955d650b41e241c6d3dfca6a9139abc05039fa2b
Stephen Quoll <stephenquoll@gmail.com>
2025-06-05 16:45:34 +1000

    First attempt

--- cmd/_dbxroot/cmd/dd-to-disk.go
+++ cmd/_dbxroot/cmd/dd-to-disk.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"os/exec"
-	"time"
+	"path/filepath"
+	"strings"
 
 	"github.com/dogeorg/dogeboxd/cmd/_dbxroot/utils"
 	dogeboxd "github.com/dogeorg/dogeboxd/pkg"
@@ -74,33 +74,73 @@ Example:
 		log.Printf("Using %s as source boot media", bootMediaDisk)
 		log.Printf("Installing to target disk: %s", targetDisk)
 
-		utils.RunCommand("sudo", "dd", "if="+bootMediaDisk.Name, "of="+targetDisk, "bs=8M", "status=progress")
+		// Create partition table
+		utils.RunParted(disk, "mklabel", "gpt")
 
-		time.Sleep(5 * time.Second)
+		utils.RunParted(disk, "mkpart", "uboot", "16384s", "24575s")
+		utils.RunParted(disk, "type", "1", "F808D051-1602-4DCD-9452-F9637FEFC49A")
 
-		utils.RunCommand("sudo", "partprobe", targetDisk)
+		utils.RunParted(disk, "mkpart", "misc", "24576s", "32767s")
+		utils.RunParted(disk, "type", "2", "C6D08308-E418-4124-8890-F8411E3D8D87")
 
-		time.Sleep(5 * time.Second)
+		utils.RunParted(disk, "mkpart", "dtbo", "32768s", "40959s")
+		utils.RunParted(disk, "type", "3", "2A583E58-486A-4BD4-ACE4-8D5454E97F5C")
 
-		// Once we've copied the data, we need to find the new partition, mount it, and set/remove some flags.
-		rootPartition, err := getWrittenRootPartition(targetDisk)
-		if err != nil {
-			log.Printf("Failed to get written root partition: %v", err)
-			os.Exit(1)
+		utils.RunParted(disk, "mkpart", "resource", "40960s", "73727s")
+		utils.RunParted(distk, "type", "4", "6115F139-4F47-4BAF-8D23-B6957EAEE4B3")
+
+		utils.RunParted(disk, "mkpart", "kernel", "73728s", "155647s")
+		utils.RunParted(disk, "type", "5", "A83FBA16-D354-45C5-8B44-3EC50832D363")
+
+		utils.RunParted(disk, "mkpart", "boot", "155648s", "221183s")
+		utils.RunParted(disk, "type", "6", "500E2214-B72D-4CC3-D7C1-8419260130F5")
+
+		utils.RunParted(disk, "mkpart", "recovery", "221184s", "286719s")
+		utils.RunParted(disk, "type", "7", "E099DA71-5450-44EA-AA9F-1B771C582805")
+
+		utils.RunParted(disk, "mkpart", "rootfs")
+		utils.RunParted(disk, "type", "8", "AF12D156-5D5B-4EE3-B415-8D492CA12EA9")
+		utils.RunParted(disk, "set", "8", "boot", "on")
+		utils.RunParted(disk, "set", "8", "legacy_boot", "on")
+
+		utils.RunCommand("dd", "if="+bootMediaDisk.Name, "of="+targetDisk, "skip=64", "seek=64", "bs=100k", "count=4", "status=progress")
+
+		hasPartitionPrefix := strings.HasPrefix(disk, "/dev/nvme") || strings.HasPrefix(disk, "/dev/mmcblk")
+		partitionPrefix := ""
+
+		if hasPartitionPrefix {
+			partitionPrefix = "p"
 		}
+
+		rootPartition := fmt.Sprintf("%s%s8", disk, partitionPrefix)
+
+		utils.RunCommand("mkfs.ext4", "-L", "nixos", rootPartition)
 
 		// Create /mnt if it doesn't exist, -p will prevent error if it already exists.
 		utils.RunCommand("sudo", "mkdir", "-p", "/mnt")
 
 		utils.RunCommand("sudo", "mount", rootPartition, "/mnt")
 
+		// Copy our NixOS configuration over
+		utils.RunCommand("mkdir", "-p", "/mnt/etc/nixos/")
+		copyFiles("/etc/nixos/", "/mnt/etc/nixos/")
+
+		// Generate hardware-configuration.nix
+		utils.RunCommand("nixos-generate-config", "--root", "/mnt")
+
 		// Set an installed flag so we know not to try again.
+		utils.RunCommand("mkdir", "-p", "/mnt/opt/")
 		utils.RunCommand("sudo", "touch", "/mnt/opt/dbx-installed")
 		utils.RunCommand("sudo", "chown", "dogeboxd:dogebox", "/mnt/opt/dbx-installed")
 
-		// Remove the ro-media flag so we don't force the user to re-install.
-		utils.RunCommand("sudo", "rm", "-rf", "/mnt/opt/ro-media")
+		flakePath, err := utils.FlakePath()
+		if err != nil {
+			log.Printf("Failed to get flake path: %v", err)
+			os.Exit(1)
+		}
 
+		// Install
+		utils.RunCommand("nixos-install", "--flake", flakePath, "--no-root-passwd", "--root", "/mnt")
 		utils.RunCommand("sudo", "umount", "/mnt")
 
 		log.Println("Finished installing. Please remove installation media and reboot.")
@@ -117,38 +157,78 @@ func init() {
 	ddToDiskCmd.MarkFlagRequired("dbx-secret")
 }
 
-func getWrittenRootPartition(disk string) (string, error) {
-	cmd := exec.Command("lsblk", disk, "-o", "name,label", "--json")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to run lsblk command: %w", err)
-	}
+//func getWrittenRootPartition(disk string) (string, error) {
+//	cmd := exec.Command("lsblk", disk, "-o", "name,label", "--json")
+//	output, err := cmd.Output()
+//	if err != nil {
+//		return "", fmt.Errorf("failed to run lsblk command: %w", err)
+//	}
+//
+//	var result struct {
+//		Blockdevices []struct {
+//			Name     string `json:"name"`
+//			Label    string `json:"label"`
+//			Children []struct {
+//				Name  string `json:"name"`
+//				Label string `json:"label"`
+//			} `json:"children,omitempty"`
+//		} `json:"blockdevices"`
+//	}
+//
+//	if err := json.Unmarshal(output, &result); err != nil {
+//		return "", fmt.Errorf("failed to parse lsblk output: %w", err)
+//	}
+//
+//	for _, device := range result.Blockdevices {
+//		if device.Label == "nixos" {
+//			return "/dev/" + device.Name, nil
+//		}
+//		for _, child := range device.Children {
+//			if child.Label == "nixos" {
+//				return "/dev/" + child.Name, nil
+//			}
+//		}
+//	}
+//
+//	return "", fmt.Errorf("no partition with label 'nixos' found")
+//}
 
-	var result struct {
-		Blockdevices []struct {
-			Name     string `json:"name"`
-			Label    string `json:"label"`
-			Children []struct {
-				Name  string `json:"name"`
-				Label string `json:"label"`
-			} `json:"children,omitempty"`
-		} `json:"blockdevices"`
-	}
-
-	if err := json.Unmarshal(output, &result); err != nil {
-		return "", fmt.Errorf("failed to parse lsblk output: %w", err)
-	}
-
-	for _, device := range result.Blockdevices {
-		if device.Label == "nixos" {
-			return "/dev/" + device.Name, nil
+func copyFiles(source string, destination string) error {
+	err := filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-		for _, child := range device.Children {
-			if child.Label == "nixos" {
-				return "/dev/" + child.Name, nil
-			}
-		}
-	}
 
-	return "", fmt.Errorf("no partition with label 'nixos' found")
+		relPath, err := filepath.Rel(source, path)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(destination, relPath)
+
+		if info.IsDir() {
+			return os.MkdirAll(destPath, info.Mode())
+		}
+
+		srcFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+
+		destFile, err := os.Create(destPath)
+		if err != nil {
+			return err
+		}
+		defer destFile.Close()
+
+		_, err = io.Copy(destFile, srcFile)
+		if err != nil {
+			return err
+		}
+
+		return os.Chmod(destPath, info.Mode())
+	})
+
+	return err
 }
