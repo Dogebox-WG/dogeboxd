b8ce3e10e05ea4514aa5c171105d173315c75ef0
Ando <divinegod@gmail.com>
2025-07-29 11:07:47 +1000

    make sure to run mount-data-overlay before dogeboxd

--- pkg/system/nix/templates/storage-overlay.nix
+++ pkg/system/nix/templates/storage-overlay.nix
@@ -5,7 +5,12 @@
 
   systemd.services.mount-data-overlay = {
     description = "Mounts the selected storage device as an overlay at {{.DATA_DIR}}";
+    before = [ "dogeboxd.service" ];
     wantedBy = [ "local-fs.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
     script = ''
       if ! ${pkgs.util-linux}/bin/mountpoint -q {{ .DATA_DIR }}; then
         ${pkgs.util-linux}/bin/mount {{ .STORAGE_DEVICE }} {{ .DATA_DIR }}
