27dc363645e156d798a2aa9c0455441c31ac56b1
Stephen Quoll <stephenquoll@gmail.com>
2025-08-21 18:34:27 +1000

    Adding SetPackageVersion

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,10 @@
 package version
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/carlmjohnson/versioninfo"
@@ -58,4 +61,63 @@ func GetDBXRelease() *DBXVersionInfo {
 			Dirty:  versioninfo.DirtyBuild,
 		},
 	}
+}
+
+func SetPackageVersion(pkg string, rev string) (bool, error) {
+	metaDir := "/opt/versioning/" + pkg
+
+	// Check 'rev' and 'hash' files exist for this package
+	if _, err := os.Stat(metaDir + "/rev"); err != nil {
+		return false, err
+	}
+
+	if _, err := os.Stat(metaDir + "/hash"); err != nil {
+		return false, err
+	}
+
+	// TODO : check hash and rev match? Check git checkout is clean?
+
+	// Retrieve hash value from system flake.lock
+	cmd := exec.Command("nix", "flake", "metadata", "/etc/nixos", "--json")
+	output, err := cmd.Output()
+	if err != nil {
+		return false, err
+	}
+
+	// We don't know what the package name will be at compile time, so this is
+	// to traverse the JSON structure at runtime
+	var data map[string]any
+	if err = json.Unmarshal([]byte(output), &data); err != nil {
+		return false, err
+	}
+
+	locks := data["locks"].(map[string]any)
+	nodes := locks["nodes"].(map[string]any)
+	pkgStruct := nodes[pkg].(map[string]any)
+	locked := pkgStruct["locked"].(map[string]any)
+	hash := locked["narHash"].(string)
+
+	if jsonRev := locked["rev"].(string); jsonRev != rev {
+		return false, fmt.Errorf("rev requested doesn't match rev in system flake.lock")
+	}
+
+	// write current 'rev' and 'hash' to 'prevRev' and 'prevHash'
+	if err = os.Rename(metaDir+"/rev", metaDir+"/prevRev"); err != nil {
+		return false, err
+	}
+
+	if err = os.Rename(metaDir+"/hash", metaDir+"/prevHash"); err != nil {
+		return false, err
+	}
+
+	// write out new 'rev' and 'hash'
+	if err = os.WriteFile(metaDir+"/hash", []byte(rev), 0644); err != nil {
+		return false, err
+	}
+
+	if err = os.WriteFile(metaDir+"/hash", []byte(hash), 0644); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
