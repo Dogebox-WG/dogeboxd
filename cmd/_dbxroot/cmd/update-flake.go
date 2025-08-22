87e6835cea50b893e08249df9ef0e0e68a033654
Stephen Quoll <stephenquoll@gmail.com>
2025-08-22 15:17:31 +1000

    Adding test version of update-flake

---
+++ cmd/_dbxroot/cmd/update-flake.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var updateFlakeCmd = &cobra.Command{
+	Use:   "update-flake",
+	Short: "Executes nix update-flake",
+	Run: func(cmd *cobra.Command, args []string) {
+		//impure, _ := cmd.Flags().GetString("impure")
+		// TODO : Do we want to support flakes other than /etc/nixos?
+
+		updateFlakeCmd := exec.Command("nix", "update-flake", "/etc/nixos")
+		updateFlakeCmd.Stdout = os.Stdout
+		updateFlakeCmd.Stderr = os.Stderr
+		err = updateFlakeCmd.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing : %v\n", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	nixCmd.AddCommand(updateFlakeCmd)
+}
