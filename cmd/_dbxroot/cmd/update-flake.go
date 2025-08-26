a3889a9e0c1c46d5f8cff71983508a49d3ef4ca0
Stephen Quoll <stephenquoll@gmail.com>
2025-08-26 14:45:02 +1000

    Updating syntax

--- cmd/_dbxroot/cmd/update-flake.go
+++ cmd/_dbxroot/cmd/update-flake.go
@@ -10,12 +10,12 @@ import (
 
 var updateFlakeCmd = &cobra.Command{
 	Use:   "update-flake",
-	Short: "Executes nix update-flake",
+	Short: "Executes nix update-flake on /etc/nixos",
 	Run: func(cmd *cobra.Command, args []string) {
 		//impure, _ := cmd.Flags().GetString("impure")
 		// TODO : Do we want to support flakes other than /etc/nixos?
 
-		updateFlakeCmd := exec.Command("nix", "update-flake", "/etc/nixos")
+		updateFlakeCmd := exec.Command("nix", "flake", "update", "--flake", "/etc/nixos")
 		updateFlakeCmd.Stdout = os.Stdout
 		updateFlakeCmd.Stderr = os.Stderr
 		err := updateFlakeCmd.Run()
