ec9f0b40b341d708f1a9bf6cc6057e582f4e090e
Stephen Quoll <stephenquoll@gmail.com>
2025-09-10 12:58:38 +1000

    Changes to use remote repository

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,10 +1,7 @@
 package version
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -29,7 +26,7 @@ type DBXVersionInfo struct {
 
 func GetDBXRelease() *DBXVersionInfo {
 	release := "unknown"
-	
+
 	// Allow override for testing
 	versionPath := "/opt/versioning"
 	if overridePath := os.Getenv("VERSION_PATH_OVERRIDE"); overridePath != "" {
@@ -70,7 +67,7 @@ func GetDBXRelease() *DBXVersionInfo {
 	}
 }
 
-func SetPackageVersion(pkg string, rev string) (bool, error) {
+func SetPackageVersion(pkg string, rev string, hash string) (bool, error) {
 	metaDir := "/opt/versioning/" + pkg
 
 	// Check 'rev' and 'hash' files exist for this package
@@ -82,47 +79,21 @@ func SetPackageVersion(pkg string, rev string) (bool, error) {
 		return false, err
 	}
 
-	// TODO : check hash and rev match? Check git checkout is clean?
-
-	// Retrieve hash value from system flake.lock
-	cmd := exec.Command("nix", "flake", "metadata", "/etc/nixos", "--json")
-	output, err := cmd.Output()
-	if err != nil {
-		return false, err
-	}
-
-	// We don't know what the package name will be at compile time, so this is
-	// to traverse the JSON structure at runtime
-	var data map[string]any
-	if err = json.Unmarshal([]byte(output), &data); err != nil {
-		return false, err
-	}
-
-	locks := data["locks"].(map[string]any)
-	nodes := locks["nodes"].(map[string]any)
-	pkgStruct := nodes[pkg].(map[string]any)
-	locked := pkgStruct["locked"].(map[string]any)
-	hash := locked["narHash"].(string)
-
-	if jsonRev := locked["rev"].(string); jsonRev != rev {
-		return false, fmt.Errorf("rev requested doesn't match rev in system flake.lock")
-	}
-
 	// write current 'rev' and 'hash' to 'prevRev' and 'prevHash'
-	if err = os.Rename(metaDir+"/rev", metaDir+"/prevRev"); err != nil {
+	if err := os.Rename(metaDir+"/rev", metaDir+"/prevRev"); err != nil {
 		return false, err
 	}
 
-	if err = os.Rename(metaDir+"/hash", metaDir+"/prevHash"); err != nil {
+	if err := os.Rename(metaDir+"/hash", metaDir+"/prevHash"); err != nil {
 		return false, err
 	}
 
 	// write out new 'rev' and 'hash'
-	if err = os.WriteFile(metaDir+"/rev", []byte(rev), 0644); err != nil {
+	if err := os.WriteFile(metaDir+"/rev", []byte(rev), 0644); err != nil {
 		return false, err
 	}
 
-	if err = os.WriteFile(metaDir+"/hash", []byte(hash), 0644); err != nil {
+	if err := os.WriteFile(metaDir+"/hash", []byte(hash), 0644); err != nil {
 		return false, err
 	}
 
